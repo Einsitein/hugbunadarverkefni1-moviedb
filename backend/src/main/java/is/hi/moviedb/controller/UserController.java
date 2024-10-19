6a5f0609a6fa6b90b4d47780fda84ce055f80c6d
Eidur <ek040803@gmail.com>
2024-10-19 13:00:43 +0000

    indentations

--- backend/src/main/java/is/hi/moviedb/controller/UserController.java
+++ backend/src/main/java/is/hi/moviedb/controller/UserController.java
@@ -7,135 +7,135 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 class RegisterRequest {
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 class LoginRequest {
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 class ChangePasswordRequest {
-  private String accessToken;
-  private String newPassword;
+    private String accessToken;
+    private String newPassword;
 
-  public String getAccessToken() {
-    return accessToken;
-  }
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
-  }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 
-  public String getNewPassword() {
-    return newPassword;
-  }
+    public String getNewPassword() {
+        return newPassword;
+    }
 
-  public void setNewPassword(String newPassword) {
-    this.newPassword = newPassword;
-  }
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
 
 class GetMeRequest {
-  private String accessToken;
+    private String accessToken;
 
-  public String getAccessToken() {
-    return accessToken;
-  }
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
-  }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  @Autowired
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-    String result = userService.register(request.getEmail(), request.getPassword());
-    if (result.equals("User already registered!")) {
-      return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
-    return new ResponseEntity<>(result, HttpStatus.CREATED);
-  }
 
-  @PostMapping("/login")
-  public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-    String result = userService.login(request.getEmail(), request.getPassword());
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-  @PatchMapping("/password")
-  public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
-    String result = userService.changePassword(request.getAccessToken(), request.getNewPassword());
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-  @DeleteMapping("/me")
-  public ResponseEntity<String> deleteMe(
-    @RequestBody GetMeRequest request
-  ) {
-    String result = userService.deleteUser(request.getAccessToken());
-    if (result = "Invalid access token!") {
-      return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        String result = userService.register(request.getEmail(), request.getPassword());
+        if (result.equals("User already registered!")) {
+            return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 
-  @GetMapping("/me")
-  public ResponseEntity<User> getMe( @RequestBody GetMeRequest request){
-    User result = userService.getUser(request.getAccessToken());
-    if (result == null) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        String result = userService.login(request.getEmail(), request.getPassword());
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  } 
 
-  @GetMapping("/users")
-  public ResponseEntity<List<User>> getAllUsers() {
-    List<User> result = userService.getAllUsers();
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+    @PatchMapping("/password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
+        String result = userService.changePassword(request.getAccessToken(), request.getNewPassword());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<String> deleteMe(
+        @RequestBody GetMeRequest request
+    ) {
+        String result = userService.deleteUser(request.getAccessToken());
+        if (result = "Invalid access token!") {
+            return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getMe( @RequestBody GetMeRequest request){
+        User result = userService.getUser(request.getAccessToken());
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    } 
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> result = userService.getAllUsers();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
