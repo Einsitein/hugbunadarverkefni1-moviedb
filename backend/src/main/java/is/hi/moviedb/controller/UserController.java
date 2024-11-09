9bc8a442a73e7950dd7ca6e23c19095f99a62c87
Eidur <ek040803@gmail.com>
2024-11-09 18:30:04 +0000

    identation

--- backend/src/main/java/is/hi/moviedb/controller/UserController.java
+++ backend/src/main/java/is/hi/moviedb/controller/UserController.java
@@ -12,91 +12,91 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 class RegisterRequest {
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 class LoginRequest {
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 class ChangePasswordRequest {
-  private String accessToken;
-  private String newPassword;
+    private String accessToken;
+    private String newPassword;
 
-  public String getAccessToken() {
-    return accessToken;
-  }
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
-  }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 
-  public String getNewPassword() {
-    return newPassword;
-  }
+    public String getNewPassword() {
+        return newPassword;
+    }
 
-  public void setNewPassword(String newPassword) {
-    this.newPassword = newPassword;
-  }
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
 
 class GetMeRequest {
-  private String accessToken;
+    private String accessToken;
 
-  public String getAccessToken() {
-    return accessToken;
-  }
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
-  }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
 
 class EmailRequest {
-  private String email;
+    private String email;
 
-  // Getters and setters
-  public String getEmail() {
-    return email;
-  }
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
 
 /**
@@ -112,159 +112,159 @@ class EmailRequest {
 @RequestMapping("/user")
 public class UserController {
 
-  private final UserService userService;
-  private final ReviewService reviewService;
+    private final UserService userService;
+    private final ReviewService reviewService;
 
 
-  @Autowired
-  public UserController(UserService userService, ReviewService reviewService) {
-    this.userService = userService;
-    this.reviewService = reviewService;
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-    String result = userService.register(request.getEmail(), request.getPassword());
-    if (result.equals("User already registered!")) {
-      return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+    @Autowired
+    public UserController(UserService userService, ReviewService reviewService) {
+        this.userService = userService;
+        this.reviewService = reviewService;
     }
-    return new ResponseEntity<>(result, HttpStatus.CREATED);
-  }
 
-  @PostMapping("/login")
-  public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-    String result = userService.login(
-      request.getEmail(),
-      request.getPassword()
-    );
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        String result = userService.register(request.getEmail(), request.getPassword());
+        if (result.equals("User already registered!")) {
+            return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
 
-  @PatchMapping("/password")
-  public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
-    String result = userService.changePassword(
-      request.getAccessToken(),
-      request.getNewPassword()
-    );
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        String result = userService.login(
+            request.getEmail(),
+            request.getPassword()
+        );
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-  /**
+    @PatchMapping("/password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
+        String result = userService.changePassword(
+            request.getAccessToken(),
+            request.getNewPassword()
+        );
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
    * Deltes currently logged in user
    *
    * @return a response endity that indicates if account was deleted.
    */
-  @DeleteMapping("/me")
-  public ResponseEntity<String> deleteMe(
-    @RequestHeader("Authorization") String accessToken
-  ) {
-    if (accessToken.startsWith("Bearer ")) {
-      accessToken = accessToken.substring(7);
+    @DeleteMapping("/me")
+    public ResponseEntity<String> deleteMe(
+        @RequestHeader("Authorization") String accessToken
+    ) {
+        if (accessToken.startsWith("Bearer ")) {
+            accessToken = accessToken.substring(7);
+        }
+        String result = userService.deleteUser(accessToken);
+        if (result == "Invalid access token!") {
+            return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    String result = userService.deleteUser(accessToken);
-    if (result == "Invalid access token!") {
-      return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
-    }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 
 
-  /**
+    /**
    * Retrieves a responseEntity with all information about User
    *
    * @return a responseEntity<User> for the currently logged in user
    */
-  @GetMapping("/me")
-  public ResponseEntity<User> getMe(
-    @RequestHeader("Authorization") String accessToken
-  ) {
-    if (accessToken.startsWith("Bearer ")) {
-      accessToken = accessToken.substring(7);
-    }
-    System.out.println("Access Token: " + accessToken);
-    User result = userService.getMe(accessToken);
-    if (result == null) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  } 
+    @GetMapping("/me")
+    public ResponseEntity<User> getMe(
+        @RequestHeader("Authorization") String accessToken
+    ) {
+        if (accessToken.startsWith("Bearer ")) {
+            accessToken = accessToken.substring(7);
+        }
+        System.out.println("Access Token: " + accessToken);
+        User result = userService.getMe(accessToken);
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    } 
 
 
-  /**
+    /**
    * Retrieves a list of all users.
    *
    * @return a list of ResponseEntity<User> objects.
    */
-  @GetMapping("/users")
-  public ResponseEntity<List<User>> getAllUsers() {
-    List<User> result = userService.getAllUsers();
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> result = userService.getAllUsers();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-  /**
+    /**
    * Retrieves a list of all Reviews a given user has created.
    *
    * @return a list of ResponseEntity<List<User>> objects.
    */
-  @GetMapping("/users/{user_id}/movies/ratings")
-  public ResponseEntity<List<Review>> getAllRatingsOfUser(
-    @PathVariable String user_id
-  ) {
-    List<Review> result = reviewService.findByUserId(Long.parseLong(user_id));
-    if(result == null){
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-  @PostMapping("/idbyemail")
-  public ResponseEntity<Long> getEmailById(@RequestBody EmailRequest emailRequest) {
-    Long result = userService.getUserId(emailRequest.getEmail());
-    if (result == null) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-
-  @GetMapping("/me/movies/ratings/avg")
-  public ResponseEntity<Double> getAverageRatingOfMe(
-    @RequestHeader("Authorization") String accessToken
-  ){
-    User me = getMe(accessToken).getBody();
-
-    List<Review> reviews = reviewService.findByUserId(me.getId());
-    if (reviews == null){
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/users/{user_id}/movies/ratings")
+    public ResponseEntity<List<Review>> getAllRatingsOfUser(
+        @PathVariable String user_id
+    ) {
+        List<Review> result = reviewService.findByUserId(Long.parseLong(user_id));
+        if(result == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    double result = reviewService.calculateAverageRating(reviews); 
-    if (result < 0.0) { 
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } 
+    @PostMapping("/idbyemail")
+    public ResponseEntity<Long> getEmailById(@RequestBody EmailRequest emailRequest) {
+        Long result = userService.getUserId(emailRequest.getEmail());
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 
-  /**
+    @GetMapping("/me/movies/ratings/avg")
+    public ResponseEntity<Double> getAverageRatingOfMe(
+        @RequestHeader("Authorization") String accessToken
+    ){
+        User me = getMe(accessToken).getBody();
+
+        List<Review> reviews = reviewService.findByUserId(me.getId());
+        if (reviews == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        double result = reviewService.calculateAverageRating(reviews); 
+        if (result < 0.0) { 
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } 
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
    * Retrieves the average rating of given User
    *
    * @return
    */
-  @GetMapping("/users/{user_id}/movies/ratings/avg")
-  public ResponseEntity<Double> getAverageRatingOfUser(
-    @PathVariable String user_id
-  ){
-    List<Review> reviews = reviewService.findByUserId(Long.parseLong(user_id));
-    if (reviews == null){
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/users/{user_id}/movies/ratings/avg")
+    public ResponseEntity<Double> getAverageRatingOfUser(
+        @PathVariable String user_id
+    ){
+        List<Review> reviews = reviewService.findByUserId(Long.parseLong(user_id));
+        if (reviews == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        double result = reviewService.calculateAverageRating(reviews); 
+        if (result < 0.0) { 
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } 
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
-    double result = reviewService.calculateAverageRating(reviews); 
-    if (result < 0.0) { 
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } 
-
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 }
--- backend/src/main/java/is/hi/moviedb/service/implementations/ReviewServiceImpl.java
+++ backend/src/main/java/is/hi/moviedb/service/implementations/ReviewServiceImpl.java
@@ -11,15 +11,15 @@ import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements ReviewService{
-  private ReviewRepository reviewRepository;
+    private ReviewRepository reviewRepository;
 
 
-  @Autowired
-  public ReviewServiceImpl(ReviewRepository reviewRepository) {
-    this.reviewRepository = reviewRepository;
-  }
+    @Autowired
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
 
-  /**
+    /**
      * Creates a new Review
      * @param userId
      * @param movieId
@@ -27,34 +27,34 @@ public class ReviewServiceImpl implements ReviewService{
      * @param rating
      * @return Review
      */
-  @Override
-  public Review createReview(
-    long userId,
-    long movieId,
-    String movieReview,
-    double rating
-  ){
-    Review review = new Review(userId,movieId,movieReview,rating);
-    reviewRepository.save(review);
-    return review;
-  }
+    @Override
+    public Review createReview(
+        long userId,
+        long movieId,
+        String movieReview,
+        double rating
+    ){
+        Review review = new Review(userId,movieId,movieReview,rating);
+        reviewRepository.save(review);
+        return review;
+    }
 
-  /**
+    /**
      * Deletes Review with id
      * @param id
      * @return true if successfull, false if not
      */
 
-  @Transactional
-  public boolean deleteReview(String id){
-    if (reviewRepository.existsById(id)) {
-      reviewRepository.deleteById(id);
-      return true; // Successfully deleted
+    @Transactional
+    public boolean deleteReview(String id){
+        if (reviewRepository.existsById(id)) {
+            reviewRepository.deleteById(id);
+            return true; // Successfully deleted
+        }
+        return false; // Review not found
     }
-    return false; // Review not found
-  }
 
-  /**
+    /**
      * Changes a particular Review
      * @param userId
      * @param movieId
@@ -63,125 +63,125 @@ public class ReviewServiceImpl implements ReviewService{
      * @return Review
      */
 
-  @Override
-  public Review changeReview(long userId,long movieId,String movieReview,double rating){
-    Review review = reviewRepository.findById(Long.toString(userId) + "-" + Long.toString(movieId));
-    review.setRating(rating);
-    review.setMovieReview(movieReview);
-    reviewRepository.save(review);
-    if (review != null){
-      return review;
-    }
-    return null;
-  };
+    @Override
+    public Review changeReview(long userId,long movieId,String movieReview,double rating){
+        Review review = reviewRepository.findById(Long.toString(userId) + "-" + Long.toString(movieId));
+        review.setRating(rating);
+        review.setMovieReview(movieReview);
+        reviewRepository.save(review);
+        if (review != null){
+            return review;
+        }
+        return null;
+    };
 
-  /**
+    /**
      * Finds a review with userId and movieId
      * @param userId
      * @param movieId
      * @return Review
      */
-  @Override
-  public Review findByUserIdAndMovieId(long userId,long movieId){
-    Review review = reviewRepository.findByUserIdAndMovieId(userId,movieId);
-    if (review != null){
-      return review;
+    @Override
+    public Review findByUserIdAndMovieId(long userId,long movieId){
+        Review review = reviewRepository.findByUserIdAndMovieId(userId,movieId);
+        if (review != null){
+            return review;
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
+    /**
      * Finds a Review by id
      * @param id
      * @return Review
      */
-  @Override
-  public Review findById(String id){
-    Review review = reviewRepository.findById(id);
-    if (review != null){
-      return review;
+    @Override
+    public Review findById(String id){
+        Review review = reviewRepository.findById(id);
+        if (review != null){
+            return review;
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
+    /**
      * Finds all reviews that a user has made
      * @param userId
      * @return List of Reviews
      */
-  @Override
-  public List<Review> findByUserId(long userId){
-    List<Review> reviews = reviewRepository.findByUserId(userId);
-    if (reviews != null){
-      return reviews;
+    @Override
+    public List<Review> findByUserId(long userId){
+        List<Review> reviews = reviewRepository.findByUserId(userId);
+        if (reviews != null){
+            return reviews;
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
+    /**
      * Finds all reviews for a particular movie
      * @param movieId
      * @return List of Reviews
      */
-  @Override
-  public List<Review> findByMovieId(long movieId){
-    List<Review> reviews = reviewRepository.findByMovieId(movieId);
-    if (reviews != null){
-      return reviews;
+    @Override
+    public List<Review> findByMovieId(long movieId){
+        List<Review> reviews = reviewRepository.findByMovieId(movieId);
+        if (reviews != null){
+            return reviews;
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
+    /**
      * Finds all Reviews
      * @return List of Reviews
      */
-  @Override
-  public List<Review> findAll(){
-    List<Review> reviews = reviewRepository.findAll();
-    if (reviews != null){
-      return reviews;
-    }
-    return null;
-  };
+    @Override
+    public List<Review> findAll(){
+        List<Review> reviews = reviewRepository.findAll();
+        if (reviews != null){
+            return reviews;
+        }
+        return null;
+    };
 
-  /**
+    /**
      * Deletes all Reviews
      * @return true if successfull, false if not
      */
 
-  @Override
-  public boolean deleteAll(){
-    if (reviewRepository.findAll() != null) {
-      reviewRepository.deleteAll();
-      return true;
-    } else {
-      return false;
+    @Override
+    public boolean deleteAll(){
+        if (reviewRepository.findAll() != null) {
+            reviewRepository.deleteAll();
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 
-  /**
+    /**
      * Finds the rating for a Review
      * @param id
      * @return Double rating, negetive if not found
      */
-  @Override
-  public double findRatingById(String id){
-    Review review = findById(id);
-    if (review != null){
-      return review.getRating();
-    } else {
-      return -1.0;
+    @Override
+    public double findRatingById(String id){
+        Review review = findById(id);
+        if (review != null){
+            return review.getRating();
+        } else {
+            return -1.0;
+        }
     }
-  }
 
-  /**
+    /**
      * Calculates the average rating from a list of reviews
      * @param List<Review> reviews
      * @return Double averageRating, negetive number if not found
      */
-  @Override
-  public double calculateAverageRating(List<Review> reviews){
-    return reviews.stream().mapToDouble(review -> review.getRating()).average().orElse(-1.0); 
-  }
+    @Override
+    public double calculateAverageRating(List<Review> reviews){
+        return reviews.stream().mapToDouble(review -> review.getRating()).average().orElse(-1.0); 
+    }
 }
